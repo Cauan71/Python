f3fdb6e379047a1f1fb036b66037c238cc1194cc
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-04-08 10:31:20 +0000

    Curso em Vídeo

---
+++ 2.Guanabara/01-Operadores/07 - Dollar.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cls || clear")
+
+dollar = float = 3.27
+
+valor = int(input("Digite o valor em R$ a ser convertido em Dollar $ : "))
+conversao = valor*dollar
+
+os.system("cls|| clear")
+
+print("====== Conversão =======")
+print("Valor Convertido: $",conversao)
