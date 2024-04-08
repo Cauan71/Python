9334817d2330d0a4a6bb77641c3735b67984c767
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-04-08 01:15:48 +0000

    Curso em Vídeo

---
+++ 2.Guanabara/01-Operadores/04 - média.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls || clear")
+
+num1 = int(input("Digite a Primeira Nota: "))
+num2 = int(input("Digite a segunda Nota: "))
+
+soma = int = num1+num2
+media = int = soma/2
+
+print("Soma : ",soma)
+print("Média: ",media)
---
+++ 2.Guanabara/01-Operadores/05 - Metros.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cls || clear")
+
+numero = int(input("Digite um Número em Metros: "))
+
+cm = numero*100
+mili = numero*1000
+
+print("Número Digitado: ",numero)
+
+print("\nCentímetros: ",cm)
+print("Milímetros: ",mili)
