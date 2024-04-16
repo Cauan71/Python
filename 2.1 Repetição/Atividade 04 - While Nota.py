d53ddfdfcbac6dca7660ad9eb37e01710ca41c82
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-04-16 18:46:56 +0000

    Lógica

--- 2.1 Repetição/Atividade 04 - While Nota.py
+++ 2.1 Repetição/Atividade 04 - While Nota.py
@@ -1,9 +1,20 @@
 import os
 
 os.system("clear || cls")
-nota: float
 
-while nota <0 and nota > 10:
-    nota = float(input("Digite sua nota: "))
+#forçando a variável a entrar no while
+nota =11
 
-print("Nota: ",nota)
+soma : float = 0
+media: float = 0
+
+while nota <0 or nota > 10:
+    for i in range(1,3):
+        nota = float(input("Digite sua {}ª Nota: ".format(i)))
+        soma += nota      
+        
+        media = soma/i
+        
+
+print("Soma: {}".format(soma))
+print("Média: {}".format(media))
