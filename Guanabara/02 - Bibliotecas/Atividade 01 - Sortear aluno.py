e20a3ebdf17ccdf4ceb64236583c57f28b5cb8fd
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-04-18 00:04:58 +0000

    Curo em Vídeo

---
+++ Guanabara/02 - Bibliotecas/Atividade 01 - Sortear aluno.py
@@ -0,0 +1,25 @@
+import random
+import os
+
+os.system("cls || clear")
+
+
+n1 = str(input("Digite o primeiro nome: "))
+n2 = str(input("Digite o segundo nome: "))
+n3 = str(input("Digite o terceiro nome: "))
+n4 = str(input("Digite o quarto nome: "))
+
+lista = [n1,n2,n3,n4]
+
+escolhido = random.choice(lista)
+
+print("Aluno(a) escolhido(a): {}".format(escolhido))
+
+
+#embaralhar ordem dos alunos
+'''
+random.shuffle(lista)
+print("Ordem de Apresentação: ")
+print(lista)
+
+'''
