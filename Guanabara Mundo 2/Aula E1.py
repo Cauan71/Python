b617813eb2c2559ee34481716c580cc6cfdc6b5b
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-05-27 23:42:47 +0000

    Curso em Vídeo

--- Guanabara Mundo 2/Aula E1.py
+++ Guanabara Mundo 2/Aula E1.py
@@ -7,24 +7,15 @@ valor = float(input("Digite o valor da casa: "))
 salario = float(input("Digite o valor do salário: "))
 anos = int(input("Digite a quantidade de anos que irá pagar: "))
 
-tempo = anos * 12
-taxa = 0.3
+prestacao =  valor / (anos * 12)
+minimo = salario * 30 / 100
 
-pagamento_mensal = valor / tempo
-porcentagem = salario * taxa 
-
-
-
-os.system("cls || clear")
-print("===== Curso em Vídeo =====")
-
-if pagamento_mensal > porcentagem:
-    print("Empréstimo foi reprovado !! ")
-else: 
-    print("Parabéns, Empréstimo foi aprovado!! ")
+if prestacao <= minimo:
+    print("Empréstimo aprovado !!")
+else:
+    print("Empréstimo negado!! ")
 
 print("Valor da Casa: {}".format(valor))
-print("Quantidade de Anos : {}".format(anos))
-print("Salário do Usuário : {}\n".format(salario))
-print("Pagamento Mensal : {:.2f}".format(pagamento_mensal))
-print("Valor Máximo que o usuário conseguirá pagar: {}".format(porcentagem))
+print("Quantidade de Anos: {}".format(anos))
+print("Valor da Prestação : {:.2f}".format(prestacao))
+print("Minimo: {}".format(minimo))
