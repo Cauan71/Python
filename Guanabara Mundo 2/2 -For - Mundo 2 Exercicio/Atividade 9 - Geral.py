c30780ce998e1a5e28055746daf442f40a891201
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-06-30 00:45:34 +0000

    Curso em Vídeo

--- Guanabara Mundo 2/2 -For - Mundo 2 Exercicio/Atividade 9 - Geral.py
+++ Guanabara Mundo 2/2 -For - Mundo 2 Exercicio/Atividade 9 - Geral.py
@@ -1,18 +1,39 @@
 import os
 
 os.system("cls || clear")
-lista = []
+soma:int = 0 
+media:int = 0 
+maior_idade_homem :int = 0
+nome_velho = ''
+contador_mulher: int = 0
 
-for i in range(2):
-    print(f"\n===== {i+1}ª Pessoa =====")
+for i in range(1,5):
+    print(f"\n===== {i}ª Pessoa =====")
     nome = input("Digite seu nome: ")
     idade = int(input("Digite sua idade: "))
-    sexo = input("Sexo [M / F]: ")
-
-    lista.append(idade)
+    sexo = input("Sexo [M / F]: ").upper()
 
     soma = soma + idade
-    media = soma/2   
+    media = soma / i
+
+    if i == 1 and sexo in 'Mm':
+        maior_idade_homem = idade 
+        nome_velho = nome
+    
+    if sexo in 'Mm' and idade > maior_idade_homem:
+        maior_idade_homem = idade
+        nome_velho = nome 
 
 
-print("Média : {}".format(media))
+
+    if sexo == "F" and idade < 20:
+        contador_mulher = contador_mulher + 1
+
+
+print("\nMédia de Idade: {}".format(media))
+print("Mulheres abaixo de 20 Anos: {}".format(contador_mulher))
+print("{} é o homem mais velho , e tem {} anos ".format(nome_velho, maior_idade_homem))
+
+
+
+
