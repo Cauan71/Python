49345582b1812c585e757f1b0d2a96794156f75c
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-07-03 23:24:29 +0000

    Curso em Vídeo

--- Guanabara Mundo 2/3 - While/Atividade 1 - M or F.py
+++ Guanabara Mundo 2/3 - While/Atividade 1 - M or F.py
@@ -2,9 +2,13 @@ import os
 
 os.system("cls || clear")
 
-sexo = "M" , "F"
-while opcao != sexo:
-    opcao = input("Digite seu sexo [ M / F]: ").upper()
+sexo = input("Digite seu sexo [ M / F]: ").upper()
+
+while sexo not in "MmFf":
+    sexo = str(input("Dados Inválidos. Informe novamente: ")).strip().upper()[0]
+    
+
+print("Sexo {} cadastrado !!".format(sexo))
 
 
 
