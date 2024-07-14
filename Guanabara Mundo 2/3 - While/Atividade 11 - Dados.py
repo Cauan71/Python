752476b14e570c0922eefd8e56f7c0b291a70695
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-07-14 14:31:35 +0000

    Curso em Vídeo

--- Guanabara Mundo 2/3 - While/Atividade 11 - Dados.py
+++ Guanabara Mundo 2/3 - While/Atividade 11 - Dados.py
@@ -6,27 +6,28 @@ contador_idade = 0
 contador_sexo = 0
 mulheres_cont = 0
 
-print("="*5,"Curso em Vídeo","="*5)
 opcao = "S"
 
 while opcao == "S":
+    print("="*5,"Curso em Vídeo","="*5)
     nome = str(input("\nDigite seu nome: "))
     idade = int(input("Digite sua idade: "))
-    sexo = str(input("Digite seu sexo: [ M / F ]")).upper().strip()[0]
+    sexo = str(input("Digite seu sexo [ M / F ]: ")).upper().strip()[0]
 
-    opcao = str(input("Deseja continuar ? [ S / N ]")).upper().strip()[0]
+    opcao = str(input("Deseja continuar [ S / N ]? ")).upper().strip()[0]
     
     os.system("cls || clear")
 
     if idade > 18:
         contador_idade += 1
 
-    if sexo == "Mm":
+    if sexo == "M":
         contador_sexo += 1
 
-    if sexo == "Ff" and idade < 20:
+    if sexo == "F" and idade < 20:
         mulheres_cont += 1 
 
+print("="*5," RESULTADO ", "="*5)
 print("Pessoas com mais de 18 anos: {}".format(contador_idade))
 print("Quantidade Homens cadastrados: {}".format(contador_sexo))
 print("Quantidade de mulheres abaixo de 20 anos: {}".format(mulheres_cont))
