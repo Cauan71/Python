383d3a39b50514541a10fe16d82a2d85b71e4585
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-07-16 21:24:53 +0000

    Curso em Vídeo

--- Guanabara Mundo 2/3 - While/Atividade 11 - Dados.py
+++ Guanabara Mundo 2/3 - While/Atividade 11 - Dados.py
@@ -2,33 +2,31 @@ import os
 
 os.system("cls || clear")
 
-cont_idade = cont_mulher = cont_homem = 0
-sexo = "F"
+cont_idade = cont_homem = cont_mulher = 0 
 
 while True:
-    nome = str(input("\nDigite seu nome: "))
+
+    print("="*5, " Curso em Vídeo ", "="*5)
+    nome = str(input("Digite seu nome: "))
     idade = int(input("Digite sua idade: "))
+    sexo = str(input("Digite seu sexo [ M / F ]: ")).upper().strip()[0]
+    opcao = str(input("Deseja continuar [ S / N ]: ")).upper().strip()[0]
 
-    while sexo not in "MF":
-        sexo = str(input("Informe seu sexo [ M / F ]: ")).upper().strip()[0]
-        
+    os.system("cls || clear")
 
-    if idade >= 18:
+    if idade > 18:
         cont_idade += 1
+
     if sexo == "M":
-        cont_homem
+        cont_homem += 1
+
     if sexo == "F" and idade < 20:
         cont_mulher += 1
-
-    opcao = ''
-
-    while opcao not in "SN":
-        opcao = str(input("Deseja continuar? [ S / N ]: "))
 
     if opcao == "N":
         break
 
-print("Pessoas acima de 18 anos: {}".format(cont_idade))
-print("Homens Cadastrados: {}".format(cont_homem))
-print("Mulheres abaixo de 20 anos: {}".format(cont_mulher))
-
+print("="*5, " Curso em Vídeo ", "="*5)
+print("Pessoas acima de 18 anos: {} ".format(cont_idade))
+print("Homens cadastrados: {} ".format(cont_homem))
+print("Mulheres abaixo de 20 anos: {} ".format(cont_mulher))
