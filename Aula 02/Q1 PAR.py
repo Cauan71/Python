ea9b9f6cd634551d0127413d64c020d003acaaa7
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2025-02-06 16:24:05 -0300

    Add files via upload

---
+++ Aula 02/Q1 PAR.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("cls || clear")
+
+num1 = int(input("Digite um número: "))
+
+if (num1 % 2 == 0):
+    print("Número Par")
+else:
+    print("Número Ímpar")
---
+++ Aula 02/Vetores.py
@@ -0,0 +1,65 @@
+import os
+
+os.system("cls || clear")
+numeros = [12,21,2,23,33,45]
+
+
+# Tipos de Vetores
+
+# Append - Insere um valor na última posição
+# Insert - Insere na posição desejada ("Posição Desejada" , "valor a ser inserido")
+# Remove - Apaga pelo Valor
+# Pop - Remove a ultima posição ou uma posição desejada
+# Sort - Ordenar um vetor
+# Len - Contar a Quantidade de Elementos de um vetor
+# Sum - Soma todos os valores do vetor
+# Max - Maior valor de um vetor
+# Min - Menor valor de um vetor
+# Random - 
+# Reverse - Inverter um Vetor
+
+
+# --------------------------------------------
+
+
+ 
+
+# contador = len(numeros)
+# numeros.append(68)
+# numeros.insert(1,67)
+# numeros.remove(33)
+# numeros.pop(4)
+# numeros.sort() 
+# numeros.reverse()
+# print(numeros)
+
+
+# soma = sum(numeros)
+# maior = max(numeros)
+# menor = min(numeros)
+# print(f"Soma {soma}, {maior}, {menor}")
+
+
+# import random
+# numeroSorteado = random.randint(1,100)
+# print(numeroSorteado)
+
+# Random Para um Vetor
+import random
+numeroSorteado = random.choice(numeros)
+
+
+
+
+# ------------------
+# SIZE = 3
+# valores = []
+
+# for i in range(SIZE):
+#     numero = int(input(f"Digite o {i+1}ª Número: "))
+#     valores.append(numero)
+    
+    
+# for i in range(SIZE):
+#      print(f"{i+1}ª Número: {valores[i]}")
+
